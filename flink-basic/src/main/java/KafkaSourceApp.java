3c8ddd5d9540c3848bb722d9e22969124d38c60d
christma <liuchuanmumumu@gmail.com>
2021-11-15 06:42:20 +0800

    add method KafkaSourceApp

---
+++ flink-basic/src/main/java/KafkaSourceApp.java
@@ -0,0 +1,29 @@
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
+import java.util.Properties;
+
+public class KafkaSourceApp {
+
+    public static void main(String[] args) throws Exception {
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "test");
+        DataStream<String> stream = env
+                .addSource(new FlinkKafkaConsumer<>("nxflink", new SimpleStringSchema(), properties));
+
+
+        stream.print();
+
+        env.execute("KafkaSource");
+    }
+}
