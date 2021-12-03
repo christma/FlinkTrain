ebe35d626fe5f2354a8da133affe0141d31c89da
christma <liuchuanmumumu@gmail.com>
2021-12-03 11:54:32 +0800

    Add FlinkKafkaConsumerApp

---
+++ flink-datastream/src/main/java/wartermark/FlinkKafkaConsumerApp.java
@@ -0,0 +1,54 @@
+package wartermark;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class FlinkKafkaConsumerApp {
+
+    public static void main(String[] args) throws Exception {
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "test");
+
+        FlinkKafkaConsumerBase<String> consumer = new FlinkKafkaConsumer<String>("nxflink", new SimpleStringSchema(), properties)
+                .assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(5)));
+
+        DataStreamSource<String> stream = env.addSource(consumer);
+
+        stream.map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String vales) throws Exception {
+                return Tuple2.of(vales, 1);
+            }
+        }).keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> tuple2) throws Exception {
+                return tuple2.f0;
+            }
+        }).reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> reduce(Tuple2<String, Integer> t1, Tuple2<String, Integer> t2) throws Exception {
+                return Tuple2.of(t1.f0, t1.f1 + t2.f1);
+            }
+        }).print();
+
+        env.execute("FlinkKafkaConsumerApp");
+
+    }
+}
