84d2aa2c993c93c323f4506a3297979aa9b3e8ec
christma <liuchuanmumumu@gmail.com>
2021-12-04 15:42:27 +0800

    Add EventTimeApp

--- flink-datastream/src/main/java/wartermark/EventTimeApp.java
+++ flink-datastream/src/main/java/wartermark/EventTimeApp.java
@@ -1,12 +1,18 @@
 package wartermark;
 
+import org.apache.commons.lang3.time.FastDateFormat;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import scala.Int;
 
 import java.util.stream.Stream;
 
@@ -26,7 +32,7 @@ public class EventTimeApp {
 
         SingleOutputStreamOperator<String> lines = env.socketTextStream("localhost", 9527)
                 .assignTimestampsAndWatermarks(
-                        new BoundedOutOfOrdernessTimestampExtractor<String>(Time.seconds(0)) {
+                        new BoundedOutOfOrdernessTimestampExtractor<String>(Time.seconds(2)) {
                             @Override
                             public long extractTimestamp(String element) {
                                 return Long.parseLong(element.split(",")[0]);
@@ -42,11 +48,34 @@ public class EventTimeApp {
             }
         });
 
+//        map.keyBy(x -> x.f0)
+//                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+//                .sum(1)
+//                .print();
+
+
         map.keyBy(x -> x.f0)
                 .window(TumblingEventTimeWindows.of(Time.seconds(5)))
-                .sum(1)
-                .print();
+                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+                    @Override
+                    public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
 
+                        System.out.println(" ---- reduce invoked ----");
+                        return Tuple2.of(value1.f0, value1.f1 + value2.f1);
+                    }
+                }, new ProcessWindowFunction<Tuple2<String, Integer>, String, String, TimeWindow>() {
+                    FastDateFormat format = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss");
+
+                    @Override
+                    public void process(String s, ProcessWindowFunction<Tuple2<String, Integer>, String, String, TimeWindow>.Context context, Iterable<Tuple2<String, Integer>> elements, Collector<String> out) throws Exception {
+                        for (Tuple2<String, Integer> element : elements) {
+                            out.collect("-----> start time :" + format.format(context.window().getStart()) + " end time :" +
+                                    format.format(context.window().getEnd()) + " element " + element.f0 + "  ===> " + element.f1);
+                        }
+                    }
+                })
+
+                .print();
 
     }
 
