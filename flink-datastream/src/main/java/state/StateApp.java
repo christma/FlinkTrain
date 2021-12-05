6038fc6550eabfa77f54fc1a34f6a677861fac49
christma <liuchuanmumumu@gmail.com>
2021-12-05 17:03:44 +0800

    Add StateApp

---
+++ flink-datastream/src/main/java/state/StateApp.java
@@ -0,0 +1,79 @@
+package state;
+
+import org.apache.flink.api.common.functions.RichFlatJoinFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StateApp {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        valueState(env);
+        env.execute("StateApp");
+    }
+
+    private static void valueState(StreamExecutionEnvironment env) {
+
+        List<Tuple2<Long, Long>> list = new ArrayList<>();
+
+
+        list.add(Tuple2.of(1L, 2L));
+        list.add(Tuple2.of(1L, 3L));
+        list.add(Tuple2.of(2L, 4L));
+        list.add(Tuple2.of(2L, 4L));
+        list.add(Tuple2.of(3L, 2L));
+        list.add(Tuple2.of(4L, 1L));
+        list.add(Tuple2.of(3L, 2L));
+        list.add(Tuple2.of(4L, 1L));
+
+        env.fromCollection(list)
+                .keyBy(value -> value.f0)
+                .flatMap(new CountWindowsAvg())
+                .print();
+
+
+    }
+
+
+    private static class CountWindowsAvg extends RichFlatMapFunction<Tuple2<Long, Long>, Tuple2<Long, Long>> {
+
+        private transient ValueState<Tuple2<Long, Long>> sum;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            ValueStateDescriptor<Tuple2<Long, Long>> descriptor = new ValueStateDescriptor<>(
+                    "average", // the state name
+                    TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {
+                    }), // type information
+                    Tuple2.of(0L, 0L));
+
+            sum = getRuntimeContext().getState(descriptor);
+        }
+
+        @Override
+        public void flatMap(Tuple2<Long, Long> value, Collector<Tuple2<Long, Long>> out) throws Exception {
+
+            Tuple2<Long, Long> currentSum = sum.value();
+
+            currentSum.f0 += 1;
+            currentSum.f1 += value.f1;
+
+            sum.update(currentSum);
+
+            if (currentSum.f0 >= 2) {
+                out.collect(new Tuple2<>(value.f0, currentSum.f1 / currentSum.f0));
+                sum.clear();
+            }
+        }
+    }
+}
