2c7f304c66d1b72eadda1ce7b0ce1502289690be
christma <liuchuanmumumu@gmail.com>
2021-12-06 16:29:01 +0800

    Add MapState

--- flink-datastream/src/main/java/state/StateApp.java
+++ flink-datastream/src/main/java/state/StateApp.java
@@ -2,17 +2,21 @@ package state;
 
 import org.apache.flink.api.common.functions.RichFlatJoinFunction;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class StateApp {
     public static void main(String[] args) throws Exception {
@@ -38,10 +42,46 @@ public class StateApp {
 
         env.fromCollection(list)
                 .keyBy(value -> value.f0)
-                .flatMap(new CountWindowsAvg())
+//                .flatMap(new CountWindowsAvg())
+                .flatMap(new AvgWithMapState())
                 .print();
 
 
+    }
+
+
+    private static class AvgWithMapState extends RichFlatMapFunction<Tuple2<Long, Long>, Tuple2<Long, Double>> {
+
+
+        private transient MapState<String, Long> mapState;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+
+            MapStateDescriptor<String, Long> descriptor = new MapStateDescriptor<>("avg", String.class, Long.class);
+
+            mapState = getRuntimeContext().getMapState(descriptor);
+        }
+
+        @Override
+        public void flatMap(Tuple2<Long, Long> value, Collector<Tuple2<Long, Double>> out) throws Exception {
+
+            mapState.put(UUID.randomUUID().toString(), value.f1);
+            ArrayList<Long> elements = Lists.newArrayList(mapState.values());
+
+            if (elements.size() >= 2) {
+                long count = 0L;
+                long sum = 0L;
+                for (Long element : elements) {
+                    count++;
+                    sum += element;
+                }
+                double avg = (double) sum / count;
+                out.collect(Tuple2.of(value.f0, avg));
+                mapState.clear();
+            }
+
+        }
     }
 
 
