2facb3eac1b58a6594488d462186b0f9322bfcdb
christma <liuchuanmumumu@gmail.com>
2022-01-10 11:01:16 +0800

    Add StateBackEndApp

--- flink-datastream/src/main/java/checkpoint/StateBackEndApp.java
+++ flink-datastream/src/main/java/checkpoint/StateBackEndApp.java
@@ -1,11 +1,15 @@
 package checkpoint;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
-import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,16 +19,39 @@ public class StateBackEndApp {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(5000);
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
-                3, // number of restart attempts
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, // number of restart attempts
                 Time.of(3, TimeUnit.SECONDS) // delay
         ));
+        // 报错不自动清除 checkpointf
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
         env.setStateBackend(new FsStateBackend("file:///Users/apple/IdeaProjects/FlinkTrain/logs/checkpoint"));
 
         DataStreamSource<String> source = env.socketTextStream("localhost", 9527);
 
 
-        source.print();
+        source.map(new MapFunction<String, String>() {
+            @Override
+            public String map(String value) throws Exception {
+                if (value.contains("NN")) {
+                    throw new RuntimeException("go go go go...");
+                } else {
+                    return value.toLowerCase();
+                }
+            }
+        }).flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String value, Collector<String> out) throws Exception {
+                String[] splits = value.split(",");
+                for (String split : splits) {
+                    out.collect(split);
+                }
+            }
+        }).map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String value) throws Exception {
+                return Tuple2.of(value, 1);
+            }
+        }).keyBy(key -> key.f0).sum(1).print();
         env.execute("StateBackEndApp");
 
     }
