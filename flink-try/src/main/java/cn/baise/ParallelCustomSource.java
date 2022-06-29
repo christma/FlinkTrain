24a13da6a2633272a019320b791af02e6c997d70
christma <liuchuanmumumu@gmail.com>
2022-06-29 13:15:17 +0800

    Add Methods

---
+++ flink-try/src/main/java/cn/baise/ParallelCustomSource.java
@@ -0,0 +1,26 @@
+package cn.baise;
+
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+
+import java.util.Random;
+
+public class ParallelCustomSource implements ParallelSourceFunction<Integer> {
+
+    private Boolean running = true;
+    private final Random rd = new Random();
+
+    @Override
+    public void run(SourceContext<Integer> sourceContext) throws Exception {
+
+        while (running) {
+            sourceContext.collect(rd.nextInt());
+            Thread.sleep(1000);
+        }
+
+    }
+
+    @Override
+    public void cancel() {
+        running = false;
+    }
+}
